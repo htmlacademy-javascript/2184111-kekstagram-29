56e0887e9a5c40237642f90a78f8e47c5fd70c31
magicgooddy <sidorovdenisdmitrievich@gmail.com>
2023-06-21 16:56:58 +0500

    generating data

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,10 @@
-// Функция для проверки длины строки. Она принимает строку, которую нужно проверить,
-//  и максимальную длину и возвращает true, если строка меньше или равна указанной длине,
-//   и false, если строка длиннее. Эта функция нам пригодится для валидации формы. Примеры использования функции:
+// const { name } = require("browser-sync");
 
-// Cтрока короче 20 символовимяФункции('проверяемая строка', 20); // true
-// Длина строки ровно 18 символо имяФункции('проверяемая строка', 18); // true
-// Строка длиннее 10 символов имяФункции('проверяемая строка', 10); // false
-
-// let testString = 'проверяемая строка';
-// let maxLenght = 15;
-
-function stringChecker(testString, maxLength) {
-  return testString.length <= maxLength;
-}
+const stringChecker = (testString, maxLength) => {
+  return testString.length <= maxLength
+};
 
 console.log(stringChecker('проверяемая строка', 15));
-
 
 function checkingPalindrome(baseString) {
   let normalizedString = baseString.replaceAll(' ','').toLowerCase();
@@ -27,3 +17,108 @@ function checkingPalindrome(baseString) {
 }
 
 console.log(checkingPalindrome('Довод'));
+
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Валя',
+  'Коля',
+  'Галя',
+  'Света',
+  'Саша',
+  'Паша',
+  'Наташа',
+  'Маня',
+  'Ваня',
+  'Соня',
+  'Сергей',
+  'Матвей',
+  'Борис',
+];
+
+const DESCRIPTIONS = [
+  'Самбрэрро',
+  'На виле',
+  'Крутота',
+  'Великий Новгород',
+  'Шел по шоссе',
+  'Берлин',
+  'Выходка столетия',
+  '17 мгновений весны',
+  'Баня',
+  'Очень забавное фото',
+  'Очень крсиво',
+  'Грустное фото',
+  'Фото в стиле "Хаки"',
+  'Гудзонский ястреб',
+  'Балдышок',
+  'Восстание машин',
+  'Слоник в четырех стенах',
+  'Посудная лавка',
+  'Спорт - это жизнь',
+  'Моя прелесть',
+  'Миф о сизифе',
+  'Рефакторинг кода',
+  'Вфыпускной',
+  'Солнце мертвых',
+  'Городской полицай',
+];
+
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+const getCommentId = createIdGenerator();
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomInteger(0, elements.length - 1)];
+
+const createMessage = () => {
+  if (getRandomInteger(0, 1)) {
+    return getRandomArrayElement(MESSAGES);
+  }
+  return getRandomArrayElement(MESSAGES) + ' ' + getRandomArrayElement(MESSAGES);
+};
+
+const posts = [];
+
+for (let i = 1; i <= 25; i++) {
+  const post = {
+    id: i,
+    url: 'photos/' + i + '.jpg',
+    description: DESCRIPTIONS[i - 1],
+    likes: getRandomInteger(15, 200),
+    comments: []
+  };
+  for (let j = 1; j <= getRandomInteger(0, 30); j++) {
+    const comment = {
+      id: getCommentId(),
+      avatar: 'img/avatar-' + getRandomInteger(1, 6) + '.svg.',
+      message: createMessage(),
+      name: getRandomArrayElement(NAMES),
+    };
+    post.comments.push(comment);
+  }
+  posts.push(post);
+}
+
+console.log(posts);

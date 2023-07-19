76e203907eb1aa1a9d6de7715cff4064b2215cc0
magicgooddy <sidorovdenisdmitrievich@gmail.com>
2023-07-19 14:39:31 +0500

    done

--- js/data.js
+++ js/data.js
@@ -1,4 +1,8 @@
-import {getRandomInteger, getRandomArrayElement, createRandomIdFromRangeGenerator} from './util.js';
+import {
+  getRandomInteger,
+  getRandomArrayElement,
+  createRandomIdFromRangeGenerator,
+} from "./util.js";
 
 const PICTURE_COUNT = 25;
 const AVATAR_COUNT = 6;
@@ -6,58 +10,59 @@ const LIKE_MIN_COUNT = 15;
 const LIKE_MAX_COUNT = 200;
 const COMMENT_COUNT = 30;
 
-
 const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  "Всё отлично!",
+  "В целом всё неплохо. Но не всё.",
+  "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
+  "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
+  "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
+  "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!",
 ];
 
 const NAMES = [
-  'Валя',
-  'Коля',
-  'Галя',
-  'Света',
-  'Саша',
-  'Паша',
-  'Наташа',
-  'Маня',
-  'Ваня',
-  'Соня',
-  'Сергей',
-  'Матвей',
-  'Борис',
+  "Валя",
+  "Коля",
+  "Галя",
+  "Света",
+  "Саша",
+  "Паша",
+  "Наташа",
+  "Маня",
+  "Ваня",
+  "Соня",
+  "Сергей",
+  "Матвей",
+  "Борис",
 ];
 
+const bNAMES = ["Валя"];
+
 const DESCRIPTIONS = [
-  'Самбрэрро',
-  'На виле',
-  'Крутота',
-  'Великий Новгород',
-  'Шел по шоссе',
-  'Берлин',
-  'Выходка столетия',
-  '17 мгновений весны',
-  'Баня',
-  'Очень забавное фото',
-  'Очень крсиво',
-  'Грустное фото',
+  "Самбрэрро",
+  "На виле",
+  "Крутота",
+  "Великий Новгород",
+  "Шел по шоссе",
+  "Берлин",
+  "Выходка столетия",
+  "17 мгновений весны",
+  "Баня",
+  "Очень забавное фото",
+  "Очень крсиво",
+  "Грустное фото",
   'Фото в стиле "Хаки"',
-  'Гудзонский ястреб',
-  'Балдышок',
-  'Восстание машин',
-  'Слоник в четырех стенах',
-  'Посудная лавка',
-  'Спорт - это жизнь',
-  'Моя прелесть',
-  'Миф о сизифе',
-  'Рефакторинг кода',
-  'Вфыпускной',
-  'Солнце мертвых',
-  'Городской полицай',
+  "Гудзонский ястреб",
+  "Балдышок",
+  "Восстание машин",
+  "Слоник в четырех стенах",
+  "Посудная лавка",
+  "Спорт - это жизнь",
+  "Моя прелесть",
+  "Миф о сизифе",
+  "Рефакторинг кода",
+  "Вфыпускной",
+  "Солнце мертвых",
+  "Городской полицай",
 ];
 
 let getCommentId = createRandomIdFromRangeGenerator(1, 1000);
@@ -66,24 +71,25 @@ const createMessage = () => {
   if (getRandomInteger(0, 1)) {
     return getRandomArrayElement(MESSAGES);
   }
-  return getRandomArrayElement(MESSAGES) + ' ' + getRandomArrayElement(MESSAGES);
+  return (
+    getRandomArrayElement(MESSAGES) + " " + getRandomArrayElement(MESSAGES)
+  );
 };
-
 
 const createPosts = () => {
   const posts = [];
   for (let i = 1; i <= PICTURE_COUNT; i++) {
     const post = {
       id: i,
-      url: 'photos/' + i + '.jpg',
+      url: "photos/" + i + ".jpg",
       description: DESCRIPTIONS[i - 1],
       likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
-      comments: []
+      comments: [],
     };
     for (let j = 1; j <= getRandomInteger(0, COMMENT_COUNT); j++) {
       const comment = {
         id: getCommentId(),
-        avatar: 'img/avatar-' + getRandomInteger(1, AVATAR_COUNT) + '.svg',
+        avatar: "img/avatar-" + getRandomInteger(1, AVATAR_COUNT) + ".svg",
         message: createMessage(),
         name: getRandomArrayElement(NAMES),
       };
@@ -98,4 +104,4 @@ createPosts();
 
 console.log(createPosts());
 
-export {createPosts};
+export { createPosts };
